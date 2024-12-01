40654d298eec6760925fada9448d3eefb5e02f3e
Niklaas Wriedt <wriedt.niklaas@gmail.com>
2024-12-01 14:54:11 +0100

    improved readability and structure, also added a mainPage with options

--- SQLite/TKinterMain.py
+++ SQLite/TKinterMain.py
@@ -1,66 +1,120 @@
 #!/usr/bin/env python3
 import tkinter as tk
 from tkinter import ttk
-from typing import Dict
 
 import addFoodEntry
 
 
-root = tk.Tk()
 # Create Vars
-count = tk.DoubleVar(); protein = tk.DoubleVar(); food = tk.StringVar()
-def init_ui():
-    root.title("Tkinter Window Demo")
-    message = tk.Label(root, text="GoodBye, Cruel World?")
-    message.pack()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    root.geometry(f"{screen_width}x{screen_height}")
-    # root.iconphoto(False, tk.PhotoImage(file='Burn.png'))
-    create_input_fields()
-    create_buttons()
 
-def create_input_fields():
-    # Create Input fields
-    food_name = ttk.Entry(root, textvariable=food)
-    food_name.pack(fill="x", expand=True)
-    calories_count = ttk.Entry(root, textvariable=count)
-    calories_count.pack(fill="x", expand=True)
-    protein_count = ttk.Entry(root, textvariable=protein)
-    protein_count.pack(fill="x", expand=True)
-    
-def create_buttons():
-    add_food_entry_button = ttk.Button( # Create Button to enter an entry into the database
-        root,
-        text="addFoodEntry",
-        command=lambda: addFoodEntry.add_entry(update_data())
-    )
-    
-    add_food_entry_button.pack( # Set visual data of above button
-        ipadx=5,
-        ipady=5,
-        expand=True
-    )
-    
-    exit_button = ttk.Button( # Create Button to exit the program
-        root,
-        text='Exit',
-        command=lambda: root.quit()
-    )
-    
-    exit_button.pack( # Set visual data of above button
-        ipadx=20,
-        ipady=20,
-        expand=True
-    )
-    
-# Take the data from the input and put it into an array for easier sql comprehension
-def update_data():
-    data: dict[str, float | str] = {"Count": count.get(), "Protein": protein.get(), "Food": food.get()}
-    return data
 
-# Initialize complete UI
-init_ui()
+class MainProgram(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title("Tkinter Window Demo")
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        self.geometry(f"{screen_width}x{screen_height}")
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        windowList = {MainPage, FoodEntry}
+        self.frames = {}
+        # Create Button to enter an entry into the database
+        for page in windowList:
+            page_name = page.__name__
+            frame = page(parent=container, controller= self)
+            self.frames[page_name] = frame
 
-# Here to keep the program running indefinitely
-root.mainloop()
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame("MainPage")
+    
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+    
+        
+
+class MainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        go_to_FoodEntry = tk.Button(self, text = "Make an entry to the database", 
+                                    command=lambda: controller.show_frame("FoodEntry"))
+
+        # Set visual data of above button
+        go_to_FoodEntry.pack(
+            ipadx=5,
+            ipady=5,
+            expand=True
+        )
+        
+        # Create Button to exit the program
+        exit_button = ttk.Button(
+            self,
+            text='Exit',
+            command=lambda: self.quit()
+        )
+
+        # Set visual data of above button
+        exit_button.pack(
+            ipadx=20,
+            ipady=20,
+            expand=True
+        )
+        
+        go_to_FoodEntry.pack()
+
+class FoodEntry(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+
+        count = tk.DoubleVar(); protein = tk.DoubleVar(); food = tk.StringVar()
+        
+        # Create Entry fields
+        food_name = ttk.Entry(self, textvariable=food)
+        calories_count = ttk.Entry(self, textvariable=count)
+        protein_count = ttk.Entry(self, textvariable=protein)
+        
+        # Put Entry fields on the layout
+        food_name.pack(fill="x", expand=True)
+        calories_count.pack(fill="x", expand=True)
+        protein_count.pack(fill="x", expand=True)
+        
+        # Create Button to enter an entry into the database
+        add_food_entry_button = ttk.Button(
+            self,
+            text="Enter",
+            command=lambda: addFoodEntry.add_entry({"Count": count.get(), "Protein": protein.get(), "Food": food.get()})
+        )
+        
+        # Set visual data of above button
+        add_food_entry_button.pack(
+            ipadx=5,
+            ipady=5,
+            expand=True
+        )
+        
+        # Create Button to go back to Main the program
+        exit_button = ttk.Button(
+            self,
+            text='Back',
+            command=lambda: controller.show_frame("MainPage")
+        )
+        
+        # Set visual data of above button
+        exit_button.pack(
+            ipadx=20,
+            ipady=20,
+            expand=True
+        )
+    
+
+# Initialize the program and keep it running until manually closing
+if __name__ == "__main__":
+    main_page = MainProgram()
+    main_page.mainloop()
--- SQLite/addFoodEntry.py
+++ SQLite/addFoodEntry.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 import sqlite3
-import argparse
-from tkinter import *
-from tkinter import ttk
 
-
-#parser = argparse.ArgumentParser()
-#parser.add_argument('count', type = float )
-#parser.add_argument('protein', type = float )
-#parser.add_argument('--food', default = "Air", type = str)
-#args = parser.parse_args()
 kalorienDB = sqlite3.connect("Kalorien.db")
 
 cursor = kalorienDB.cursor()
-#dataInput = {"Count": args.count, "Protein": args.protein, "Food": args.food}
 def add_entry(data):
     print(data["Count"])
     cursor.execute("INSERT INTO Kalorien(count, protein, date, food) VALUES (:Count, :Protein, current_date, :Food )", data)
@@ -22,3 +12,4 @@ def add_entry(data):
     print(f"Amount of calories consumed today\n{cursor.fetchall()}")
     cursor.execute("SELECT SUM(protein) FROM Kalorien WHERE date=current_date")
     print(f"Amount of proteins consumed today:\n{cursor.fetchall()}")
+
